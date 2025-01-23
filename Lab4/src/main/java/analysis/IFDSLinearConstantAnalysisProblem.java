6e582c2f9c7ced5381a5e05eddebc24efb34ee52
Maxim Kuznetsov <max.kuznetsov@yahoo.com>
2025-01-23 18:17:21 +0100

    Added lab4 files

---
+++ Lab4/src/main/java/analysis/IFDSLinearConstantAnalysisProblem.java
@@ -0,0 +1,95 @@
+package analysis;
+
+import heros.DefaultSeeds;
+import heros.FlowFunction;
+import heros.FlowFunctions;
+import heros.InterproceduralCFG;
+import heros.flowfunc.Identity;
+import heros.solver.Pair;
+import sootup.analysis.interprocedural.ifds.DefaultJimpleIFDSTabulationProblem;
+import sootup.core.jimple.basic.Local;
+import sootup.core.jimple.common.stmt.Stmt;
+import sootup.core.model.SootMethod;
+import sootup.core.signatures.MethodSignature;
+import sootup.core.types.NullType;
+import sootup.java.core.views.JavaView;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class IFDSLinearConstantAnalysisProblem extends DefaultJimpleIFDSTabulationProblem<Pair<Local, Integer>, InterproceduralCFG<Stmt, SootMethod>> {
+    private final List<MethodSignature> entryPoints;
+
+    protected final JavaView view;
+
+    protected final static int LOWER_BOUND = -1000;
+
+    protected final static int UPPER_BOUND = 1000;
+
+    protected InterproceduralCFG<Stmt, SootMethod> icfg;
+
+    public IFDSLinearConstantAnalysisProblem(List<MethodSignature> entryPoints, JavaView view, InterproceduralCFG<Stmt, SootMethod> icfg) {
+        super(icfg);
+        this.entryPoints = entryPoints;
+        this.view = view;
+        this.icfg = icfg;
+    }
+
+    @Override
+    public Map<Stmt, Set<Pair<Local, Integer>>> initialSeeds() {
+        for (MethodSignature methodSignature : entryPoints) {
+            SootMethod m = view.getMethod(methodSignature).get();
+            if (!m.hasBody()) {
+                continue;
+            }
+            if (m.getName().equals("entryPoint")) {
+                return DefaultSeeds.make(Collections.singleton(m.getBody().getStmtGraph().getStartingStmt()), zeroValue());
+            }
+        }
+        throw new IllegalStateException("View does not contain entryPoint " + entryPoints);
+    }
+
+
+    // TODO: You have to implement the FlowFunctions interface.
+    // Use Pair<Local, Integer> as type for the data-flow facts.
+    @Override
+    protected FlowFunctions<Stmt, Pair<Local, Integer>, SootMethod> createFlowFunctionsFactory() {
+        return new FlowFunctions<Stmt, Pair<Local, Integer>, SootMethod>() {
+            @Override
+            public FlowFunction<Pair<Local, Integer>> getNormalFlowFunction(Stmt curr, Stmt next) {
+                // TODO: Implement this flow function factory to obtain an intra-procedural data-flow analysis.
+                return Identity.v();
+            }
+
+            @Override
+            public FlowFunction<Pair<Local, Integer>> getCallFlowFunction(Stmt callsite, SootMethod dest) {
+                // TODO: Implement this flow function factory to map the actual into the formal arguments.
+                // Caution, actual parameters may be integer literals as well.
+                return Identity.v();
+            }
+
+            @Override
+            public FlowFunction<Pair<Local, Integer>> getReturnFlowFunction(Stmt callsite, SootMethod callee, Stmt exit, Stmt retsite) {
+                // TODO: Map the return value back into the caller's context if applicable.
+                // Since Java has pass-by-value semantics for primitive data types, you do not have to map the formals
+                // back to the actuals at the exit of the callee.
+                return Identity.v();
+            }
+
+            @Override
+            public FlowFunction<Pair<Local, Integer>> getCallToReturnFlowFunction(Stmt callsite, Stmt retsite) {
+                // TODO: getCallToReturnFlowFunction can be left to return id in many analysis; this time as well?
+                return Identity.v();
+            }
+        };
+    }
+
+
+    @Override
+    protected Pair<Local, Integer> createZeroValue() {
+        return new Pair<>(new Local("<<zero>>", NullType.getInstance()), Integer.MIN_VALUE);
+    }
+
+}
